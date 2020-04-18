82150a05841054b67e4e87fb0571908b29002715
InkoHX <inkohx@gmail.com>
2020-04-18 17:42:02 +0900

    fix(GuildMember): Get members using fetch

--- src/decorators/command/types/GuildMember.ts
+++ src/decorators/command/types/GuildMember.ts
@@ -4,12 +4,12 @@ import { ArgumentResolverFunction } from '.'
 
 const idRegex = /<@!?(?<id>\d{17,19})>/gu
 
-const toGuildMember: ArgumentResolverFunction = (data, paramIndex, language, message): GuildMember => {
+const toGuildMember: ArgumentResolverFunction = async (data, paramIndex, language, message): Promise<GuildMember> => {
   const id = idRegex.exec(String(data))?.groups?.id
 
   if (!id) throw new Error(language.error.resolver.guildMember(paramIndex))
 
-  const member = message.guild?.member(id)
+  const member = await message.guild?.members.fetch(id)
 
   if (!member) throw new Error(language.error.resolver.guildMember(paramIndex))
 
